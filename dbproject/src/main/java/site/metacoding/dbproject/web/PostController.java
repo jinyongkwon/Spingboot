0d4fab1d4b6b61531e61c7594f13e5db09c0d13e
JY <JY@naver.com>
2022-03-08 16:49:50 +0900

    12.글쓰기 완료

--- dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
+++ dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -1,5 +1,7 @@
 package site.metacoding.dbproject.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,16 +9,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import lombok.RequiredArgsConstructor;
+import site.metacoding.dbproject.domain.post.Post;
+import site.metacoding.dbproject.domain.post.PostRepository;
+import site.metacoding.dbproject.domain.user.User;
+
+@RequiredArgsConstructor // final이 붙은 애들에 대한 생성자를 만들어준다.
 @Controller
 public class PostController {
 
-    // GET 글쓰기 페이지 /post/writeForm
-    @GetMapping("/post/writeForm")
+    private final HttpSession session;
+    private final PostRepository postRepository;
+
+    // GET 글쓰기 페이지 /post/writeForm - 인증 O
+    @GetMapping("/s/post/writeForm")
     public String writeForm() {
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
         return "post/writeForm";
     }
 
-    // 메인페이지
+    // 메인페이지 - 인증 X
     // GET 글목록 페이지 /post/list, /
     // @GetMapping({"/", "/post/list"})
     @GetMapping({ "/", "/post/list" })
@@ -24,33 +38,41 @@ public class PostController {
         return "post/list";
     }
 
-    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨)
-    @GetMapping("/post/{id}")
+    // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨) - 인증 X
+    @GetMapping("/post/{id}") // Get요청에 /post 제외 시키기
     public String detail(@PathVariable Integer id) {
         return "post/detail";
     }
 
-    // GET 글수정 페이지 /post/{id}/updateForm
-    @GetMapping("/post/{id}/updateForm")
+    // GET 글수정 페이지 /post/{id}/updateForm - 인증 O
+    @GetMapping("/s/post/{id}/updateForm")
     public String updateForm(@PathVariable Integer id) {
         return "post/updateForm"; // ViewResolver 도움 받음.
     }
 
-    // DELETE 글삭제 /post/{id} - 글목록으로 가기
-    @DeleteMapping("/post/{id}")
+    // DELETE 글삭제 /post/{id} - 글목록으로 가기 - 인증 O
+    @DeleteMapping("/s/post/{id}")
     public String delete(@PathVariable Integer id) {
         return "redirect:/";
     }
 
-    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기
-    @PutMapping("/post/{id}")
+    // UPDATE 글수정 /post/{id} - 글상세보기 페이지가기 - 인증 O
+    @PutMapping("/s/post/{id}")
     public String update(@PathVariable Integer id) {
         return "redirect:/post/" + id;
     }
 
-    // POST 글쓰기 /post - 글목록으로 가기
-    @PostMapping("/post")
-    public String write() {
+    // POST 글쓰기 /post - 글목록으로 가기 - 인증 O
+    @PostMapping("/s/post")
+    public String write(Post post) {
+        // title, content 1.null검사, 2. 공백검사, 3.길이검사 .......
+        if (session.getAttribute("principal") == null) {
+            return "redirect:/loginForm";
+        }
+        User principal = (User) session.getAttribute("principal");
+        post.setUser(principal); // user의 id만 들어감
+        // insert into post(title, content, userid) values(사용자, 사용자, 세션오브젝트의 PK)
+        postRepository.save(post);
         return "redirect:/";
     }
 }
