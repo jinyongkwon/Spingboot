32603fdd9ed67d61e33e77824fb1647840438f8d
JY <JY@naver.com>
2022-03-09 11:37:19 +0900

    14. 글상세보기 페이지 완료

--- dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
+++ dbproject/src/main/java/site/metacoding/dbproject/web/PostController.java
@@ -1,6 +1,7 @@
 package site.metacoding.dbproject.web;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
@@ -47,8 +48,16 @@ public class PostController {
 
     // GET 글상세보기 페이지 /post/{id} (삭제버튼 만들어 두면됨, 수정버튼 만들어 두면됨) - 인증 X
     @GetMapping("/post/{id}") // Get요청에 /post 제외 시키기
-    public String detail(@PathVariable Integer id) {
-        return "post/detail";
+    public String detail(@PathVariable Integer id, Model model) {
+        Optional<Post> postOp = postRepository.findById(id);
+
+        if (postOp.isPresent()) {// 박스안에 값이 있으면 = 선물!!
+            Post postEntity = postOp.get(); // 선물이있으면 값을 넣음.
+            model.addAttribute("post", postEntity);
+            return "post/detail";
+        } else {
+            return "error/page1"; // DB를 억지로 날리지 않는이상 여기로 올수가없음.
+        }
     }
 
     // GET 글수정 페이지 /post/{id}/updateForm - 인증 O
